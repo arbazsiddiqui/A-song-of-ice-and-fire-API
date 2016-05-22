b785a41baa222a61768b148d4bcd352182064ff1
arbazsiddiqui <arbaz00@gmail.com>
2016-05-22 21:13:00 +0530

    implemented all the getters for book

--- asoiaf.js
+++ asoiaf.js
@@ -14,6 +14,26 @@ var asoiaf = function() {
         var httpGet = $.get;
     }
 
+    //Getters for book
+
+    exports.getBookByID = function(book_id) {
+        book_id = book_id.toString();
+        var url = 'http://www.anapioficeandfire.com/api/books/' + book_id;
+        return httpGet(url);
+    }
+
+    exports.getBookByName = function(book_name) {
+        var url = 'http://www.anapioficeandfire.com/api/books/?name=' + book_name;
+        return httpGet(url);
+    }
+
+    exports.getAllBooks = function() {
+        var url = 'http://www.anapioficeandfire.com/api/books';
+        return httpGet(url);
+    }
+
+    //Getters for Character
+
     exports.getCharacterByID = function(char_id) {
         char_id = char_id.toString();
         var url = 'http://www.anapioficeandfire.com/api/characters/' + char_id;
@@ -24,5 +44,10 @@ var asoiaf = function() {
         var url = 'http://www.anapioficeandfire.com/api/characters/?name=' + char_name;
         return httpGet(url);
     }
-    
+
+    exports.getAllCharacters = function() {
+        var url = 'http://www.anapioficeandfire.com/api/characters';
+        return httpGet(url);
+    }
+
 }();
